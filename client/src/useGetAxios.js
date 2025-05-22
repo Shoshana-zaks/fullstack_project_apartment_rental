0151207ad524a85b3eb4471e88b462c3e4cd98e4
Shoshana Zaks <syrzaks@example.com>
2025-05-22 23:00:42 +0300

    תיקון baseUrl לכל הפניות לשרת לפי סביבה

--- client/src/useGetAxios.js
+++ client/src/useGetAxios.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+const baseUrl = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3001/';
+
 export const useGetAxios = (url) => {
     const [data, setData] = useState([]);
     const [error, setError] = useState();
 
     useEffect(() => {
-        axios.get(url)
+        axios.get(baseUrl + url)
             .then(x => {
                 setData(x.data);
             })
@@ -18,43 +20,37 @@ export const useGetAxios = (url) => {
     return { data, error };
 };
 
-const baseUrl = `http://localhost:3001/`;
-
-export const registerUser = async (advertiser) => { // שינוי שם הפונקציה לשם יותר מתאים
+export const registerUser = async (advertiser) => {
     try {
         const response = await axios.post(`${baseUrl}advertiser/signin`, advertiser, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
+            headers: { 'Content-Type': 'application/json' }
         });
         return response;
     } catch (error) {
         console.error("Error during registration:", error);
-        throw error; // חשוב להמשיך את השגיאה כדי שהקומפוננטה תוכל לטפל בה
+        throw error;
     }
 };
 
-export const loginUser = async (credentials) => { // שינוי שם הפונקציה לשם יותר מתאים
+export const loginUser = async (credentials) => {
     try {
         const response = await axios.post(`${baseUrl}advertiser/login`, credentials, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
+            headers: { 'Content-Type': 'application/json' }
         });
         return response;
     } catch (error) {
         console.error("Error during login:", error);
-        throw error; // חשוב להמשיך את השגיאה כדי שהקומפוננטה תוכל לטפל בה
+        throw error;
     }
 };
 
 export const addApartment = async (apartment) => {
     try {
-        const token = localStorage.getItem('Authorization'); // קבלת הטוקן רק כשצריך (לאחר התחברות)
+        const token = localStorage.getItem('Authorization');
         const response = await axios.post(`${baseUrl}apartment/addApartment`, apartment, {
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}` // שליחת הטוקן עם הבקשה
+                'Authorization': `Bearer ${token}`
             }
         });
         return response;
@@ -64,10 +60,5 @@ export const addApartment = async (apartment) => {
     }
 };
 
-export const allCity = () => {
-    return axios.get(`${baseUrl}city`);
-};
-
-export const allCategory = () => {
-    return axios.get(`${baseUrl}category`);
-};
+export const allCity = () => axios.get(`${baseUrl}city`);
+export const allCategory = () => axios.get(`${baseUrl}category`);
