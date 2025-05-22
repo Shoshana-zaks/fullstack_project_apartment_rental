0d697a9a9cb31f7ab7aab87cb16e850daed9e608
Shoshana Zaks <syrzaks@example.com>
2025-05-22 23:02:29 +0300

    תיקון baseUrl לכל הפניות לשרת לפי סביבה

--- client/src/api.js
+++ client/src/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3001',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3001',
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${localStorage.getItem('Authorization')}`, // Send JWT
-  }
+    'Authorization': `Bearer ${localStorage.getItem('Authorization')}`,
+  },
 });
 
 export default api;
